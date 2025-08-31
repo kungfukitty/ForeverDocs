34d5b698575220df9df96650181930b4435e461d
kungfukitty <46254915+kungfukitty@users.noreply.github.com>
2025-08-31 11:05:53 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// src/App.tsx
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
@@ -7,6 +8,8 @@ import SiteFooter from './components/SiteFooter'
 import WaitingListPage from './pages/WaitingList'
 import SponsorPage from './pages/Sponsor'
 import AmbassadorPage from './pages/Ambassador'
+import ImpactPage from './pages/Impact'
+import PressPage from './pages/Press'
 
 export default function App() {
   return (
@@ -19,6 +22,8 @@ export default function App() {
           <Route path="/waiting-list" element={<WaitingListPage />} />
           <Route path="/sponsor" element={<SponsorPage />} />
           <Route path="/ambassador" element={<AmbassadorPage />} />
+          <Route path="/impact" element={<ImpactPage />} />
+          <Route path="/press" element={<PressPage />} />
         </Routes>
       </div>
       <SiteFooter />
